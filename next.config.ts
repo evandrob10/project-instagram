c398b3c6b58ed3b290dcbc0cfd0fbea21592e43a
Evandro Barros <evandrobarrosb98@gmail.com>
2025-04-16 20:36:00 -0300

    Config: url image externe, update modules, install instagra-url-direct

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['scontent-for2-1.cdninstagram.com','scontent-for2-2.cdninstagram.com']
+  },
 };
 
 export default nextConfig;
