dabfc32bba53504fef8981a9b43b637dc69aacd1
Evandro Barros <evandrobarrosb98@gmail.com>
2025-04-17 17:16:55 -0300

    componente button

---
+++ src/app/Components/Download/Components/Button/index.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+interface ButtonType{
+  url: string,
+  text: string,
+  bgCor: string,
+  opacity?:string,
+  target?:string 
+}
+
+export default function Button({url , text , bgCor, opacity, target = "_parent"} : ButtonType ) {
+  return (
+    <Link href={url} target={`${target}`}>
+          <button type="button" className={`m-3 font-bold ${bgCor} ${opacity} py-2 px-3 rounded-[.2em] text-amber-50 cursor-pointer transform duration-300 hover:scale-110`}>{text}</button>
+    </Link>
+  )
+}
