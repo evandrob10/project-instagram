c2287e6faa329ddba8bf298a94136ae682bacdbb
Evandro Barros <evandrobarrosb98@gmail.com>
2025-04-17 18:06:30 -0300

    Include valid url_list > 0, config style and itens

--- src/app/Components/Download/Components/Button/index.tsx
+++ src/app/Components/Download/Components/Button/index.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 
 interface ButtonType{
-  url: string,
-  text: string,
-  bgCor: string,
-  opacity?:string,
-  target?:string 
+  url: string
+  text: string
+  bgCor: string
+  opacity?:string
+  target?:string
+  width?: string
 }
 
-export default function Button({url , text , bgCor, opacity, target = "_parent"} : ButtonType ) {
+export default function Button({url , text , bgCor, opacity, target = "_parent", width=""} : ButtonType ) {
   return (
     <Link href={url} target={`${target}`}>
-          <button type="button" className={`m-3 font-bold ${bgCor} ${opacity} py-2 px-3 rounded-[.2em] text-amber-50 cursor-pointer transform duration-300 hover:scale-110`}>{text}</button>
+          <button type="button" className={`m-3 font-bold ${bgCor} ${opacity} ${width} py-2 px-3 rounded-[.2em] text-amber-50 cursor-pointer transform duration-300 hover:scale-110`}>{text}</button>
     </Link>
   )
 }
--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -16,27 +16,56 @@ export default async function download({ searchParams }: PropsType) {
   try {
     const data = await instagramGetUrl(searchParams.url);
 
-    return (
-      <section className="flex flex-col justify-center items-center py-5">
+    if (data.url_list.length == 1) {
+      return (
+        <section className="flex flex-col justify-center items-center py-5">
 
-        <figure className="overflow-hidden">
-          <Image
-            alt=""
-            width={300}
-            height={650}
-            src={data.media_details[0].thumbnail && data.media_details[0].type !== "image" ? data.media_details[0].thumbnail : data.media_details[0].url}
-            style={{
-              objectFit: "cover",
-              transform: "scale(1.1)",
-            }}
-          />
-        </figure>
-        <div className="mt-5 w-3xs flex justify-between">
-          <Button url={"/"} bgCor="bg-gray-500" text="VOLTAR" opacity="opacity-50" />
-          <Button url={data.url_list[0]} bgCor="bg-insta-buttons" text="BAIXAR" target="_blank" />
-        </div>
-      </section>
-    )
+          <figure className="overflow-hidden">
+            <Image
+              alt=""
+              width={300}
+              height={650}
+              src={data.media_details[0].thumbnail && data.media_details[0].type !== "image" ? data.media_details[0].thumbnail : data.media_details[0].url}
+              style={{
+                objectFit: "cover",
+                transform: "scale(1.1)",
+              }}
+            />
+          </figure>
+          <div className="mt-5 w-3xs flex justify-between">
+            <Button url={"/"} bgCor="bg-gray-500" text="VOLTAR" opacity="opacity-50" />
+            <Button url={data.url_list[0]} bgCor="bg-insta-buttons" text="BAIXAR" target="_blank" />
+          </div>
+        </section>
+      )
+    } else {
+      return (
+        <section className="flex flex-col justify-center items-center py-5 mb-5">
+          <div className="flex flex-wrap justify-center items-center mb-5">
+            {data.url_list.map((element, index) => (
+              <div key={index} className="m-1">
+                <figure className="overflow-hidden">
+                  <Image
+                    alt=""
+                    width={300}
+                    height={650}
+                    src={data.media_details[index].thumbnail && data.media_details[index].type !== "image" ? data.media_details[index].thumbnail : data.media_details[index].url}
+                    style={{
+                      objectFit: "cover",
+                      transform: "scale(1.1)",
+                    }}
+                  />
+                </figure>
+                <div className="my-2 w-3xs flex justify-center">
+                  <Button url={element} bgCor="bg-insta-buttons" text="BAIXAR" target="_blank" />
+                </div>
+              </div>
+            ))}
+          </div>
+          <Button url={"/"} bgCor="bg-gray-500" text="VOLTAR" width="w-[50vw] sm:w-[10vw]" />
+        </section>
+      )
+    }
   } catch {
     redirect(`/?error=url_download`);
   }
