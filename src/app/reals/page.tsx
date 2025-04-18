5ee4cb131c0e310dfc50f1f75f1de8c616790052
Evandro Barros <evandrobarrosb98@gmail.com>
2025-04-18 13:39:34 -0300

    create pages nav

---
+++ src/app/reals/page.tsx
@@ -0,0 +1,19 @@
+//COMPONENTS:
+import ContainerDownload from "../Components/Download";
+
+interface searchParamsType{
+  searchParams:{
+    error: string
+  }
+}
+
+export default function page({searchParams} : searchParamsType) {
+  const params = searchParams.error;
+
+  return (
+    <main className="flex flex-col justify-center items-center w-full">
+      <ContainerDownload itemDownload="Reals" error={params}/>
+    </main>
+  );
+}
+
