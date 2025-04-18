cb9d4a5db4e9d3cc025bcb98bd68d5683b79ae76
Evandro Barros <evandrobarrosb98@gmail.com>
2025-04-18 13:24:33 -0300

    Include: nav and style nav

---
+++ src/app/Components/Download/Components/Nav/index.tsx
@@ -0,0 +1,49 @@
+'use client'
+ 
+import { usePathname } from 'next/navigation'
+import Link from "next/link";
+
+type NavTypes = {
+    url: string, 
+    text: string
+}[]
+
+export default function Nav() {
+
+    const pathname = usePathname()
+
+    const itemsNav : NavTypes = [
+        {
+            url: "/",
+            text: "VIDEOS"
+        },
+        {
+            url: "/imagens",
+            text: "IMAGENS"
+        },
+        {
+            url: "/reals",
+            text: "REALS"
+        },
+        {
+            url: "/multi-videos",
+            text: "MULTI VIDEOS"
+        },
+        {
+            url: "/multi-imagens",
+            text: "MULTI IMAGENS"
+        }
+    ]
+
+    return (
+        <nav>
+            <ul className="flex justify-evenly">
+                {itemsNav.map(({url,text}, index) => (
+                    <Link key={index} href={url}>
+                        <li className={`${(pathname === url) ? "border-b-3 opacity-100 font-bold" : "opacity-70"} text-insta-basic transform duration-100 hover:border-b-3 hover:scale-110`}>{text}</li>
+                    </Link>
+                ))}
+            </ul>
+        </nav>
+    )
+}
--- src/app/Components/Download/index.tsx
+++ src/app/Components/Download/index.tsx
@@ -1,31 +1,35 @@
 'use client'
+import Nav from "./Components/Nav";
 import InputUrl from "./Components/InputUrl";
 //Hooks
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 //Components next js
-import {useRouter} from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
-export default function ContainerDownload({itemDownload, error} : {itemDownload: string, error: string}) {
-    const [url ,  setUrl] = useState("");
+
+
+export default function ContainerDownload({ itemDownload, error }: { itemDownload: string, error: string }) {
+    const [url, setUrl] = useState("");
     const [urlValid, setUrlValid] = useState(false);
     const [controlError, setControlError] = useState(error === "url_download" && true);
 
     const router = useRouter();
 
-    useEffect(()=>setUrl(""),[error]);
-    useEffect(()=>{if(url.length > 0) setControlError(false);},[url])
-    useEffect(()=>{if(urlValid) router.push(`/download?url=${url}`) },[url])
-    
+    useEffect(() => setUrl(""), [error]);
+    useEffect(() => { if (url.length > 0) setControlError(false); }, [url])
+    useEffect(() => { if (urlValid) router.push(`/download?url=${url}`) }, [url])
+
     const message = <p className="text-[red] text-[18px] mt-2">URL inserida é inválida!</p>;
 
     return (
         <section className="w-[50%] h-[50%] text-center">
             <h2 className="font-bold text-4xl mb-10 text-insta-basic">Download <span className="py-1 px-2 rounded-2xl opacity-90 bg-insta-basic text-amber-50 animate-videos">{itemDownload}</span> do instagram</h2>
-            <div>
-                <InputUrl  setUrl={setUrl} authenticateUrl={setUrlValid} />
+            <div className="mb-2">
+                <InputUrl setUrl={setUrl} authenticateUrl={setUrlValid} />
             </div>
-            {(controlError) && message }
+            {(controlError) && message}
             {(!urlValid && url.length > 26) && message}
+            <Nav />
         </section>
     )
 }
