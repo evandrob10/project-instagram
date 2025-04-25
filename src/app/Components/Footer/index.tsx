1da6a460ea2bfbf1cca54ac41fda773a41be4406
Evandro Barros <evandrobarrosb98@gmail.com>
2025-04-25 19:00:49 -0300

    include politicy, term and update footer responsive

--- src/app/Components/Footer/index.tsx
+++ src/app/Components/Footer/index.tsx
@@ -2,7 +2,11 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    <footer className="text-center text-[14px] xl:text-[14px] 2xl:text-[18px] py-2 bg-[#424E60] text-[#ffffff] w-full h-[8vh] sm:h-[10vh] 2xl:h-[8vh]">
+    <footer className=" flex flex-col justify-center items-center text-center text-[14px] xl:text-[14px] 2xl:text-[18px] py-2 bg-[#424E60] text-[#ffffff] w-full h-[12vh]">
+        <div>
+          <Link href="/politica-de-privacidade" className="underline">Política de Privacidade</Link>
+          <Link href="/termos-de-servico" className="mx-3 underline" >Termos de Serviço</Link>
+        </div>
         <p>© 2025 <Link href={"/"} className="underline">InstaSaver Hub</Link>. Todos os direitos reservados. </p>
         <p>Desenvolvido por <Link href={"https://www.linkedin.com/in/evandro-barros-251026160/"} target="_blank" className="underline">Evandro Barros</Link>.</p>
     </footer>
