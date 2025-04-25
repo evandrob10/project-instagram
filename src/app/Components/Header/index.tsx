308cc978efee3f33d5df3cd2088402be56b09070
Evandro Barros <evandrobarrosb98@gmail.com>
2025-04-24 21:40:16 -0300

    remov function scrollY header

--- src/app/Components/Header/index.tsx
+++ src/app/Components/Header/index.tsx
@@ -1,26 +1,6 @@
-'use client'
 import Link from "next/link";
-//hooks
-import { useEffect, useState } from "react";
 
 export default function Header() {
-  const [scrollY, setScrollY] = useState(0);
-
-  // Efeito para capturar a rolagem
-  useEffect(() => {
-    // Função para atualizar o estado com a posição do scroll
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-    };
-
-    // Adiciona o evento de scroll
-    window.addEventListener('scroll', handleScroll);
-
-    // Limpa o evento ao desmontar o componente
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []); // O array vazio significa que o efeito será executado uma única vez (ao montar o componente)
   return (
     <header className={`flex items-center h-[8vh] justify-center landscape:static pt-5 w-full`}>
       <Link href={"/"}>
