9ffe598e31b41a70e5d01159dcb33742c0ef5062
Evandro Barros <evandrobarrosb98@gmail.com>
2025-04-25 19:13:10 -0300

    include target in links of footer

--- src/app/Components/Footer/index.tsx
+++ src/app/Components/Footer/index.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className=" flex flex-col justify-center items-center text-center text-[14px] xl:text-[14px] 2xl:text-[18px] py-2 bg-[#424E60] text-[#ffffff] w-full h-[12vh]">
         <div>
-          <Link href="/politica-de-privacidade" className="underline">Política de Privacidade</Link>
-          <Link href="/termos-de-servico" className="mx-3 underline" >Termos de Serviço</Link>
+          <Link href="/politica-de-privacidade" className="underline" target="_blank">Política de Privacidade</Link>
+          <Link href="/termos-de-servico" className="mx-3 underline" target="_blank">Termos de Serviço</Link>
         </div>
         <p>© 2025 <Link href={"/"} className="underline">InstaSaver Hub</Link>. Todos os direitos reservados. </p>
         <p>Desenvolvido por <Link href={"https://www.linkedin.com/in/evandro-barros-251026160/"} target="_blank" className="underline">Evandro Barros</Link>.</p>
