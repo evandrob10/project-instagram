538056c74aa0aba859aea4e9a4dfaa415a64a901
Evandro Barros <evandrobarrosb98@gmail.com>
2025-04-24 22:19:56 -0300

    Revised url back page

--- src/app/Components/Download/index.tsx
+++ src/app/Components/Download/index.tsx
@@ -8,15 +8,21 @@ import { useRouter } from 'next/navigation';
 
 
 
-export default function ContainerDownload({ itemDownload, error }: { itemDownload: string, error: string | null}) {
+export default function ContainerDownload({ itemDownload, error }: { itemDownload: string, error: string | null }) {
     const [url, setUrl] = useState("");
     const [urlValid, setUrlValid] = useState(false);
     const [controlError, setControlError] = useState(error === "url_download" && true);
 
+    const itemSemAcento = itemDownload
+        .replace(/í/g, "i")
+        .replace(/é/g, "e");
+
+    console.log(itemSemAcento);
+
     const router = useRouter();
 
     useEffect(() => { if (url.length > 0) setControlError(false); }, [url])
-    useEffect(() => { if (urlValid) router.push(`/download?page=${itemDownload}&url=${url}`) }, [url, urlValid, router, itemDownload])
+    useEffect(() => { if (urlValid) router.push(`/download?page=${itemSemAcento ? itemSemAcento : itemDownload}&url=${url}`) }, [url, urlValid, router, itemDownload, itemSemAcento])
 
     const message = <p className="text-[red] text-[18px] mt-3">URL inserida é inválida!</p>;
 
@@ -24,7 +30,7 @@ export default function ContainerDownload({ itemDownload, error }: { itemDownloa
         <section className="w-[90%] md:w-[60%] xl:w-[50%] min-h-[92vh] flex flex-col justify-center items-center text-center">
             <h2 className="font-bold text-4xl xl:text-5xl mb-10 text-insta-basic w-full">Download <span className="py-1 px-2 rounded-2xl opacity-90 bg-insta-basic text-amber-50 animate-videos">{itemDownload}</span> do instagram</h2>
             <div className="mb-2 w-full">
-                <InputUrl setUrl={setUrl} authenticateUrl={setUrlValid}/>
+                <InputUrl setUrl={setUrl} authenticateUrl={setUrlValid} />
             </div>
             <Nav />
             {(controlError) && message}
